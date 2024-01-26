00238170035737b2be655423be5587138a9f90ba
Francisco  Hernandez <franciscohernandez@MacBook-Pro-de-Francisco.local>
2024-01-25 23:52:28 -0600

    Validate empty value in searchTextField

--- app/src/main/java/com/mercadolibre/challenge/domain/use_case/search/Search.kt
+++ app/src/main/java/com/mercadolibre/challenge/domain/use_case/search/Search.kt
@@ -1,7 +1,9 @@
 package com.mercadolibre.challenge.domain.use_case.search
 
 import com.mercadolibre.challenge.domain.model.RequestSearch
+import com.mercadolibre.challenge.domain.model.Response
 import com.mercadolibre.challenge.domain.repository.SearchRepository
+import com.mercadolibre.challenge.domain.retrofit.search.SearchResponse
 import javax.inject.Inject
 
 /**
@@ -13,5 +15,11 @@ class Search @Inject constructor(private val repository: SearchRepository) {
      * Invoke to search in the repository
      * @param request The data class with request to search
      */
-    suspend operator fun invoke(request: RequestSearch) = repository.search(request.product)
+    suspend operator fun invoke(request: RequestSearch): Response<SearchResponse> {
+        return if (!request.product.contains(" ")) {
+            repository.search(request.product)
+        } else {
+            Response.Failure(Exception("Campo de busqueda vacio"))
+        }
+    }
 }
