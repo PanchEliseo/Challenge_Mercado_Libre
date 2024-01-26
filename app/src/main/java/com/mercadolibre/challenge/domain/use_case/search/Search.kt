1845e8cb0946a53b967ef262dfc5174a753d8034
Francisco  Hernandez <franciscohernandez@MacBook-Pro-de-Francisco.local>
2024-01-26 12:24:54 -0600

    Add regex for text format in searchTextField

--- app/src/main/java/com/mercadolibre/challenge/domain/use_case/search/Search.kt
+++ app/src/main/java/com/mercadolibre/challenge/domain/use_case/search/Search.kt
@@ -1,9 +1,7 @@
 package com.mercadolibre.challenge.domain.use_case.search
 
 import com.mercadolibre.challenge.domain.model.RequestSearch
-import com.mercadolibre.challenge.domain.model.Response
 import com.mercadolibre.challenge.domain.repository.SearchRepository
-import com.mercadolibre.challenge.domain.retrofit.search.SearchResponse
 import javax.inject.Inject
 
 /**
@@ -15,11 +13,5 @@ class Search @Inject constructor(private val repository: SearchRepository) {
      * Invoke to search in the repository
      * @param request The data class with request to search
      */
-    suspend operator fun invoke(request: RequestSearch): Response<SearchResponse> {
-        return if (!request.product.contains(" ")) {
-            repository.search(request.product)
-        } else {
-            Response.Failure(Exception("Campo de busqueda vacio"))
-        }
-    }
+    suspend operator fun invoke(request: RequestSearch) = repository.search(request.product)
 }
